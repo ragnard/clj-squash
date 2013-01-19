bcc268306096676437d6d9c3de68176e255c1f9e
Ragnar Dahlén <r.dahlen@gmail.com>
2013-01-19 11:43:27 +0000

    Add profiles for Clojure 1.3, 1.4 and 1.5-RC1

--- project.clj
+++ project.clj
@@ -6,4 +6,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.6.3"]
                  [clj-stacktrace "0.2.5"]
-                 [cheshire "5.0.1"]])
+                 [cheshire "5.0.1"]]
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
+  :aliases {"all" ["with-profile" "1.3:1.4:1.5"]})
